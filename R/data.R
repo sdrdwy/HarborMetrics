e2813d4300843429c1816ef0a16c20d968069b9b
Myonu <12090163@link.cuhk.edu.cn>
2025-05-08 14:45:43 +0800

    setup basic functions

---
+++ R/data.R
@@ -0,0 +1,14 @@
+# Reads NewYork_dailydata.csv and converts dates.
+load_port_data <- function(path){
+
+}
+
+#Handels missing values and outliers
+clean_port_data <- function(path){
+
+}
+
+#Creates features(e.g. "storm_flag" if wind > 20mph)
+add_weather_features <- function(data){
+
+}
