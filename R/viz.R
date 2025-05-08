575300cc79186d183fbb5372291e6983c3b35bb3
noobhyx <165311676+noobhyx@users.noreply.github.com>
2025-05-09 00:58:49 +0800

    Update viz.R

--- R/viz.R
+++ R/viz.R
@@ -1,14 +1,19 @@
-#Time series plot of cargo throughput vs. wind speed
-plot_operational_trend = function(data){
-
-}
-
-#Heatmap of weather cv. operational metrics
-plot_correlation_heatmap = function(data){
-
-}
-
-#Interatctive plot of predicted delayes under hypothetical weather
-simulate_scenartio = function(model, new_weather){
-    
+#' @param result result to be analyzed
+#' @export 
+result_visualization <- function(result) {
+  # 可视化训练集预测
+  train_plot <- autoplot(ts(result$actual_train)) +
+    autolayer(ts(result$train_predictions)) +
+    labs(title = "ARIMA model training set predicts the effect",
+         x = "Time index", 
+         y = "Mooring time (hours)")
+  print(train_plot)
+  
+  # 可视化测试集预测
+  test_plot <- autoplot(ts(result$actual_test)) +
+    autolayer(ts(result$test_predictions)) +
+    labs(title = "ARIMA model test set prediction effect",
+         x = "Time index", 
+         y = "Mooring time (hours)")
+  print(test_plot)
 }
